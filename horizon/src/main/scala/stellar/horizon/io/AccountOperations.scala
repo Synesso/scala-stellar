034297c4f724620b2046fe71314a35c9d1483c4e
Felix Satyaputra <fsatyaputra@zendesk.com>
2020-05-28 15:43:12 +1000

    Attempt with tagless final approach - 2

--- horizon/src/main/scala/stellar/horizon/io/AccountOperations.scala
+++ horizon/src/main/scala/stellar/horizon/io/AccountOperations.scala
@@ -14,3 +14,23 @@ object AccountOperations {
 trait AccountOperations[F[_]] {
   def accountDetail(accountId: AccountId): F[AccountDetail]
 }
+
+
+trait AllOperation[F[_]] {
+  def accountOperations: AccountOperations[F]
+  def passwordOperations: PasswordOperations[F]
+}
+
+
+object Program {
+  def resetPassword(accountId: AccountId, passwordInput: PasswordInput)
+    (implicit ops: AllOperation): Future[AccountDetail] = {
+    import ops._
+
+    for {
+      accountDetail <- accountOperations.accountDetail(accountId)
+      validatedPasswordInput <- passwordResetOperation.validate(passwordInput)
+      _ <- passwordResetOperation.reset(accountDetail, validatedPasswordInput)
+    }
+  }
+}
